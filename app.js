b31829d09d28334e7bf6e35ec452497a9c079d54
Lee-Soyeon <leesoyena@gmail.com>
2022-03-03 20:53:41 +0900

    4.2 Events

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 
-function onLoginButtonClick() {
-    const value = loginInput.value
-    
+function onLoginButtonClick(event) {
+    event.preventDefault()
+    console.log(event)
 }
 
 loginForm.addEventListener("submit", onLoginButtonClick)
