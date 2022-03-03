133019976a7ca2aedaf0f52fc532d4866b98889a
Lee-Soyeon <leesoyena@gmail.com>
2022-03-03 21:49:44 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 const greeting = document.querySelector("#greeting")
 
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+
 function onLoginButtonClick(event) {
     event.preventDefault()
     const username = loginInput.value
-    localStorage.setItem("username", username)
-    loginForm.classList.add("hidden")
-    greeting.innerText = `Hello, ${username}`
-    greeting.classList.remove("hidden")
+    localStorage.setItem(USERNAME_KEY, username)
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    paintGreeting(username)
 }
 
-loginForm.addEventListener("submit", onLoginButtonClick)
+function paintGreeting(username) {
+    greeting.innerText = `Hello, ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener("submit", onLoginButtonClick)
+} else {
+    paintGreeting(savedUsername)
+}
