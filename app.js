a4a53d256963019e9d24a29c8016f5739543d7a6
Lee Soyeon <leesoyena@gmail.com>
2022-03-03 20:47:31 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input")
+
+function onLoginButtonClick() {
+    const value = loginInput.value
+    
+}
+
+loginForm.addEventListener("submit", onLoginButtonClick)
